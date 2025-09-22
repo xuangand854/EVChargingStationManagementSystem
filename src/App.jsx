e3cf0202b98ea315233626b1220e0d944bec6654
thanhtungse192400 <gamesay00@gmail.com>
2025-09-22 15:58:43 +0700

    feat(Signup)thêm chức năng sigup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 
-import InputField from "./components/inputField";
-import SocialLogin from "./components/SocialLogin";
+import InputField from "./components/account/InputField";
+import SocialLogin from "./components/account/SocialLogin";
 const App = () => {
   return (
     <div className="Login-container">
-      <h2 className="form-title"> Login in with</h2>
+      <h2 className="form-title"><a href="/google-connection"> Login in with</a></h2>
       <SocialLogin />
 
       <p className="separator"><span>or</span></p>
 
       <form action="#" className="Login-form">
-        <InputField type ="email"placeholder="Email address"icon="mail" />
-        <InputField type ="password"placeholder="Password"icon="lock"/>
+        <InputField type="email" placeholder="Email address" icon="mail" />
+        <InputField type="password" placeholder="Password" icon="lock" />
+
         
         
         <a href="#" className="forgot-pass-link">Forgot Password?</a>
         <button className="login-button">Log In</button>
       </form>
-      <p className="signup-text">Don&apos;t have an account? <a href="#">Sign up now</a></p>
+      <p className="signup-text">Don&apos;t have an account? <a href="/sign-up">Sign up now</a></p>
     </div>
   )
 }

193641a9a42f2b14e8ba94bf9f6f262770517800
ntan0409 <ntan0409@gmail.com>
2025-09-26 18:13:38 +0700

    feat(login page)

--- src/components/account/InputField.jsx
+++ src/components/account/InputField.jsx
@@ -1,18 +1,18 @@
-import{ useState} from "react"
-const InputField = ({type,placeholder,icon}) => {
-    //State no toggle password visibility
-    const[isPasswordShown,setIsPasswordShow] = useState (false);
+import { useState } from "react"
+const InputField = ({ type, placeholder, icon, name, value, onChange }) => {
+  //State no toggle password visibility
+  const [isPasswordShown, setIsPasswordShow] = useState(false);
 
   return (
     <div className="input-wrapper">
-          <input type={isPasswordShown ? 'text' : type} placeholder={placeholder} className="input-field" required />
-          <i className="material-symbols-rounded">{icon}</i>
-          {type === 'password' && (
-            <i onClick={() => setIsPasswordShow (prevState => !prevState)} className="material-symbols-rounded eye-icon">
-                {isPasswordShown ? 'visibility' : 'visibility_off' }
-            </i>
-          )}
-        </div>
+      <input name={name} value={value} onChange={onChange} type={isPasswordShown ? 'text' : type} placeholder={placeholder} className="input-field" required />
+      <i className="material-symbols-rounded">{icon}</i>
+      {type === 'password' && (
+        <i onClick={() => setIsPasswordShow(prevState => !prevState)} className="material-symbols-rounded eye-icon">
+          {isPasswordShown ? 'visibility' : 'visibility_off'}
+        </i>
+      )}
+    </div>
   )
 }
 

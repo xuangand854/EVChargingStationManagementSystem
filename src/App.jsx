4d86ff91c534b0f72d9a22a48040f16f75536995
ntan0409 <ntan0409@gmail.com>
2025-09-30 17:30:28 +0700

    fix lỗi em TÙNG báo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Layout from "./components/pages/Layout";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
-import Login from "./components/login/Login";
+import Login from "./components/pages/Login";
 import Profile from "./components/profile/Profile";
 import Logout from "./components/pages/Logout";
 import PrivateRoute from "./components/pages/PrivateRoute";

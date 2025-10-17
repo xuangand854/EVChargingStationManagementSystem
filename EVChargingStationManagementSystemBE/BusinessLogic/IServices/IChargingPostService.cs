12ffaf5d356848ca542b010f5ac586867cb189a4
xuangand854 <dangphxse161782@fpt.edu.vn>
2025-10-18 02:14:57 +0700

    refactor(ChargingPost) add UpdateConnectorCount into ChargingPost service interface

--- EVChargingStationManagementSystemBE/BusinessLogic/IServices/IChargingPostService.cs
+++ EVChargingStationManagementSystemBE/BusinessLogic/IServices/IChargingPostService.cs
@@ -11,6 +11,7 @@ namespace BusinessLogic.IServices
         Task<IServiceResult> Create(ChargingPostCreateDto dto);
         Task<IServiceResult> Update(ChargingPostUpdateDto dto, Guid stationId);
         Task<IServiceResult> UpdateStatus(ChargingPostStatus status, Guid postId);
+        Task<IServiceResult> UpdateConnectorCount(bool toggle, Guid postId);
         Task<IServiceResult> Delete(Guid postId);
     }
 }

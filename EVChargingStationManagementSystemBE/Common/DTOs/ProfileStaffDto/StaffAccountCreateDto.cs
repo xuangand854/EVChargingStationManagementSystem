16f5b9ffe6443b7688d9f8b253cde1a01b33a898
chanqua321 <phucpvse183363@fpt.edu.vn>
2025-11-15 23:16:48 +0700

    refactor evdriver

--- EVChargingStationManagementSystemBE/Common/DTOs/ProfileStaffDto/StaffAccountCreateDto.cs
+++ EVChargingStationManagementSystemBE/Common/DTOs/ProfileStaffDto/StaffAccountCreateDto.cs
@@ -10,8 +10,11 @@ namespace Common.DTOs.ProfileStaffDto
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Mật khẩu không được để trống")]
-        [MinLength(6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự")]
+        [MinLength(8, ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\W).{8,}$",
+       ErrorMessage = "Mật khẩu phải có ít nhất 1 chữ hoa, 1 chữ thường, 1 ký tự đặc biệt và tối thiểu 8 ký tự")]
         public string Password { get; set; }
+
 
         [Required(ErrorMessage = "Xác nhận mật khẩu không được để trống")]
         [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không khớp")]
@@ -20,10 +23,10 @@ namespace Common.DTOs.ProfileStaffDto
     
         [Required(ErrorMessage = "Họ và tên không được để trống")]
         [MaxLength(100)]
-        public string Name { get; set; }   
+        public string Name { get; set; }
 
-        [Phone]
-        [MaxLength(15)]
+        [RegularExpression(@"^(?:\+84|0)(?:3[2-9]|5[2689]|7[0|6-9]|8[1-9]|9[0-9])[0-9]{7}$",
+         ErrorMessage = "Số điện thoại không hợp lệ")]
         public string PhoneNumber { get; set; }  
 
         [MaxLength(255)]

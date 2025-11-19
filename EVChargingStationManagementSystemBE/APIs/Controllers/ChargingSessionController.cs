f1edf4c112abea14f2a2aea185fe60de9d934d51
xuangand854 <dangphxse161782@fpt.edu.vn>
2025-11-20 02:14:48 +0700

    feat(Session) Implement get session by connector Id API

--- EVChargingStationManagementSystemBE/APIs/Controllers/ChargingSessionController.cs
+++ EVChargingStationManagementSystemBE/APIs/Controllers/ChargingSessionController.cs
@@ -52,6 +52,20 @@ namespace APIs.Controllers
             return StatusCode(500, new { message = result.Message });
         }
 
+        [HttpGet("Connector/{connectorId}")]
+        public async Task<IActionResult> GetByConnectorId([FromRoute] Guid connectorId)
+        {
+            var result = await _service.GetByConnector(connectorId);
+
+            if (result.Status == Const.SUCCESS_READ_CODE)
+                return Ok(new { data = result.Data, message = result.Message });
+
+            if (result.Status == Const.WARNING_NO_DATA_CODE)
+                return NotFound(new { message = result.Message });
+
+            return StatusCode(500, new { message = result.Message });
+        }
+
         [HttpPost("Start")]
         public async Task<IActionResult> Start([FromBody]ChargingSessionStartDto dto)
         {
